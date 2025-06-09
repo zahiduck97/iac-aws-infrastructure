1525e2f3a87482802b17b46529b6a70853b65762
Zahiduck <zahid.butron@gmail.com>
2025-06-08 18:44:35 -0600

    Add Terraform Cloud Backend

--- providers.tf
+++ providers.tf
@@ -1,8 +1,16 @@
 terraform {
+  cloud {
+    organization = "Zahiduck" # este es el nombre de la org que te crea terraform cloud
+
+    workspaces {
+      name = "iac-aws-infrastructure" # este es el nombre exacto que diste al workspace
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = "~> 4.0"
     }
   }
 }
